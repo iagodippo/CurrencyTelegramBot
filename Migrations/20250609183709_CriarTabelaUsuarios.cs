06bc9d9bdf7cb8b5b0f9addfa1fa5b34efe55e38
Iago D'Ippolito <iagodippolito@hotmail.com>
2025-06-09 16:10:36 -0300

    feat: create User table and update properties for user management

---
+++ Migrations/20250609183709_CriarTabelaUsuarios.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CurrencyTelegramBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarTabelaUsuarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UltimoEnvio",
+                table: "User",
+                newName: "LastNotify");
+
+            migrationBuilder.RenameColumn(
+                name: "IntervaloMinutos",
+                table: "User",
+                newName: "MinutesInterval");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "User",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "User");
+
+            migrationBuilder.RenameColumn(
+                name: "MinutesInterval",
+                table: "User",
+                newName: "IntervaloMinutos");
+
+            migrationBuilder.RenameColumn(
+                name: "LastNotify",
+                table: "User",
+                newName: "UltimoEnvio");
+        }
+    }
+}
---
+++ Migrations/20250609185429_RenomearIntervaloMinutos.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CurrencyTelegramBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenomearIntervaloMinutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
